bc513bf61b70afd176402bba41959e2ea23e32a6
何正道 <>
2019-10-09 14:12:23 +0800

    1

---
+++ HPolygonalRadar/ViewController.m
@@ -0,0 +1,140 @@
+//
+//  ViewController.m
+//  HPolygonalRadar
+//
+//  Created by 何正道 on 2019/4/19.
+//  Copyright © 2019 何正道. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CollectionViewCell.h"
+#import "HPolygonalRadarView.h"
+
+@interface ViewController ()
+<
+HPolygonalRadarViewDataSource,
+UICollectionViewDataSource,
+UICollectionViewDelegate,
+UICollectionViewDelegateFlowLayout
+> {
+    BOOL _isVertexUp;
+    NSInteger _edgeCount;
+}
+
+@property (nonatomic, strong) UICollectionView    *collectionView;
+@property (nonatomic, strong) UITableView         *tableView;
+
+@property (nonatomic, strong) HPolygonalRadarView *viRadar;
+@property (nonatomic, strong) NSMutableDictionary *randomDataSource;
+@property (nonatomic, copy  ) NSArray             *arrRadarTitles;
+@property (nonatomic, copy  ) NSArray             *arrDemoOperations;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _randomDataSource = [NSMutableDictionary dictionary];
+    _arrRadarTitles = @[@"急加速",@"急转弯",@"超速行驶",@"高峰行驶",@"夜间行驶",@"急减速"];
+    _arrDemoOperations = @[@"重绘",@"边数++",@"边数--",@"切换朝向"];
+    _isVertexUp = YES;
+    _edgeCount = 3;
+    
+    [self.view addSubview:self.viRadar];
+    [self.view addSubview:self.collectionView];
+}
+
+- (HPolygonalRadarView *)viRadar {
+    if (!_viRadar) {
+        HPolygonalRadarBgStyleConfig *bgConfig = [HPolygonalRadarBgStyleConfig new];
+        HPolygonalRadarStyleConfig *radarConfig0 = [HPolygonalRadarStyleConfig new];
+//        HPolygonalRadarStyleConfig *radarConfig1 = [HPolygonalRadarStyleConfig new];
+        radarConfig0.animationDuration = 3;
+        
+        _viRadar = [[HPolygonalRadarView alloc] initWithCenter:CGPointMake(self.view.center.x, 200)
+                                                    dataSource:self
+                                                    isVertexUp:_isVertexUp
+                                                        radius:120
+                                                     edgeCount:_edgeCount
+                                                 bgStyleConfig:bgConfig
+                                              radarStyleConfig:@[radarConfig0]];
+    }
+    return _viRadar;
+}
+
+- (UICollectionView *)collectionView {
+    if (!_collectionView) {
+        UICollectionViewFlowLayout *layout = [UICollectionViewFlowLayout new];
+        layout.sectionInset = UIEdgeInsetsMake(10, 15, 10, 15);
+        layout.minimumLineSpacing = 10;
+        layout.minimumInteritemSpacing = 10;
+        layout.itemSize = CGSizeMake(80, 30);
+        
+        _collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_viRadar.frame) + 100, self.view.bounds.size.width, self.view.bounds.size.height - CGRectGetMaxY(_viRadar.frame) - 100) collectionViewLayout:layout];
+        [_collectionView setBackgroundColor:[UIColor whiteColor]];
+        _collectionView.dataSource = self;
+        _collectionView.delegate = self;
+        [_collectionView registerClass:[CollectionViewCell class] forCellWithReuseIdentifier:NSStringFromClass([CollectionViewCell class])];
+    }
+    return _collectionView;
+}
+
+#pragma mark - HPolygonalRadarViewDataSource
+- (CGFloat)HPolygonalRadarValueAtIndex:(NSInteger)index radarIndex:(NSInteger)radarIndex {
+    NSString *key = [NSString stringWithFormat:@"%zd%zd", radarIndex, index];
+    if ([_randomDataSource valueForKey:key]) {
+        return [[_randomDataSource valueForKey:key] floatValue];
+    } else {
+        CGFloat value = (CGFloat)(arc4random() % 100 + 1) / 100;
+        [_randomDataSource setValue:@(value) forKey:key];
+        return value;
+    }
+}
+
+- (UILabel *)HPolygonalRadarTitleLabelAtIndex:(NSInteger)index {
+    UILabel *lb = [UILabel new];
+    lb.text = _arrRadarTitles[(index % _arrRadarTitles.count)];
+    lb.font = [UIFont boldSystemFontOfSize:15];
+    lb.textColor = [UIColor blackColor];
+    return lb;
+}
+
+#pragma mark - UICollectionViewDataSource & UICollectionViewDelegate
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return _arrDemoOperations.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    CollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([CollectionViewCell class]) forIndexPath:indexPath];
+    cell.lbOperation.text = _arrDemoOperations[indexPath.row];
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *operation = _arrDemoOperations[indexPath.row];
+    if ([operation isEqualToString:@"重绘"]) {
+        [self resetRadar];
+    } else if ([operation isEqualToString:@"边数++"]) {
+        _edgeCount++;
+        [self resetRadar];
+    } else if ([operation isEqualToString:@"边数--"]) {
+        if (3 < _edgeCount) {
+            _edgeCount--;
+            [self resetRadar];
+        }
+    } else if ([operation isEqualToString:@"切换朝向"]) {
+        _isVertexUp = !_isVertexUp;
+        [self resetRadar];
+    }
+}
+
+#pragma mark - 自定义方法
+- (void)resetRadar {
+    [_viRadar removeFromSuperview];
+    _viRadar = nil;
+    [_randomDataSource removeAllObjects];
+    [self.view addSubview:self.viRadar];
+}
+
+@end
